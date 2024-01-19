343481ffd493766b98daa94db52ffa8ec286e720
Cyril Arrachequesne <cyrilou564@gmail.com>
2024-01-19 13:17:11 +0100

    🔨 replace all require by import

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
 const app = express();
 
 // Basic Configuration
@@ -8,17 +11,17 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+	res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+	res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+app.listen(port, function () {
+	console.log(`Listening on port ${port}`);
 });
