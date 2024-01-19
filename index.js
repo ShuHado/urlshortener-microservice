cae5b35f615381bfed902a8566ad896306691c7c
Cyril Arrachequesne <cyrilou564@gmail.com>
2024-01-19 13:19:22 +0100

    🔨 add route to create shorturl

--- index.js
+++ index.js
@@ -9,12 +9,21 @@ const app = express();
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(cors());
 
 app.use("/public", express.static(`${process.cwd()}/public`));
 
 app.get("/", function (req, res) {
 	res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/shorturl", function (req, res) {
+	const url = req.body.url;
+	const short_url = Math.floor(Math.random() * 100);
+	res.json({ original_url: url, short_url: short_url });
 });
 
 app.listen(port, function () {
